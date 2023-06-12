1b5b8f5ba1a036d0eb1683e942e26a29251f0888
notchuks <chukaezem@gmail.com>
2023-06-12 21:13:16 +0100

    initial commit

---
+++ src/main/java/com/example/payroll/EmployeeModelAssembler.java
@@ -0,0 +1,20 @@
+package com.example.payroll;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+// This class Employee objects to EntityModel<Employee> objects, so we can display additional links
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+    @Override
+    public EntityModel<Employee> toModel(Employee employee) {
+
+        return EntityModel.of(employee,
+            linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+            linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    }
+}
